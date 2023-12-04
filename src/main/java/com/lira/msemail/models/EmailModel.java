3f4d49b5971f3610069bab5d8c11ba8d877422ef
Wanderson Lira <wandersonlira.dev1@gmail.com>
2023-12-03 23:59:34 -0300

    metodo sendEmail

--- src/main/java/com/lira/msemail/models/EmailModel.java
+++ src/main/java/com/lira/msemail/models/EmailModel.java
@@ -2,6 +2,7 @@ package com.lira.msemail.models;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import com.lira.msemail.enums.StatusEmail;
 
@@ -11,10 +12,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 
-@Data
 @Entity
 @Table(name="TB_EMAIL")
 public class EmailModel implements Serializable{
@@ -32,5 +31,108 @@ public class EmailModel implements Serializable{
 	private String text;
 	private LocalDateTime sendDateEmail;
 	private StatusEmail statusEmail;
+	
+	
+	public EmailModel() {}
+
+	public EmailModel(Long emailId, String ownerRef, String emailFrom, String emailTo, String subject, String text,
+			LocalDateTime sendDateEmail, StatusEmail statusEmail) {
+		super();
+		this.emailId = emailId;
+		this.ownerRef = ownerRef;
+		this.emailFrom = emailFrom;
+		this.emailTo = emailTo;
+		this.subject = subject;
+		this.text = text;
+		this.sendDateEmail = sendDateEmail;
+		this.statusEmail = statusEmail;
+	}
+	
+	
+	
+	public Long getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(Long emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getOwnerRef() {
+		return ownerRef;
+	}
+
+	public void setOwnerRef(String ownerRef) {
+		this.ownerRef = ownerRef;
+	}
+
+	public String getEmailFrom() {
+		return emailFrom;
+	}
+
+	public void setEmailFrom(String emailFrom) {
+		this.emailFrom = emailFrom;
+	}
+
+	public String getEmailTo() {
+		return emailTo;
+	}
+
+	public void setEmailTo(String emailTo) {
+		this.emailTo = emailTo;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public LocalDateTime getSendDateEmail() {
+		return sendDateEmail;
+	}
+
+	public void setSendDateEmail(LocalDateTime sendDateEmail) {
+		this.sendDateEmail = sendDateEmail;
+	}
+
+	public StatusEmail getStatusEmail() {
+		return statusEmail;
+	}
+
+	public void setStatusEmail(StatusEmail statusEmail) {
+		this.statusEmail = statusEmail;
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(emailId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmailModel other = (EmailModel) obj;
+		return Objects.equals(emailId, other.emailId);
+	}
+	
+	
 
 }
--- src/main/java/com/lira/msemail/services/EmailService.java
+++ src/main/java/com/lira/msemail/services/EmailService.java
@@ -1,20 +1,44 @@
 package com.lira.msemail.services;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import com.lira.msemail.enums.StatusEmail;
 import com.lira.msemail.models.EmailModel;
 import com.lira.msemail.repositories.EmailRepository;
-
 
 @Service
 public class EmailService {
 	
 	@Autowired
-	EmailRepository emailRepository;
-
-	public void sendEmail(EmailModel emailModel) {
-		// TODO Auto-generated method stub
+	private EmailRepository emailRepository;
+	@Autowired
+	private JavaMailSender emailSender;
+	
+	
+	
+	public EmailModel sendEmail(EmailModel emailModel) {
+		emailModel.setSendDateEmail(LocalDateTime.now());
+		try {
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setFrom(emailModel.getEmailFrom());
+			message.setTo(emailModel.getEmailTo());
+			message.setSubject(emailModel.getSubject());
+			message.setText(emailModel.getText());
+			emailSender.send(message);
+			
+			emailModel.setStatusEmail(StatusEmail.SENT);
+			
+		} catch (MailException e) {
+			emailModel.setStatusEmail(StatusEmail.ERROR);
+		}
+		
+		return emailRepository.save(emailModel);
 		
 	}
 	
